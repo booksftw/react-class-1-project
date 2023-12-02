93b2ecc8acfa9001c1eb74cb8e80e367be3fe322
Nicholas Zachary <39927058+booksftw@users.noreply.github.com>
2023-12-01 20:32:05 -0800

    form to app js passing data correctly

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import "./App.css";
 import { Form } from "./components/Form";
 import { TableList } from "./components/TableList";
+import logo from "./assets/investment-calculator-logo.png";
 
 function App() {
   const dataFromForm = (data) => {
-    console.log(data);
+    console.log(data, "data from child");
   };
 
   return (
     <>
-      <h1>Logo</h1>
+      <img src={logo} width="200" />
       <h1>React Investment Calculator</h1>
       <Form submitForm={dataFromForm} />
       <TableList />
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,16 +1,43 @@
 import React, { useState } from "react";
 
 export function Form(props) {
-  props.submitForm("test");
-  const [initInvestment, setInvestment] = useState();
+  const [inputs, setInputs] = useState({});
+
+  const submitForm = (event) => {
+    event.preventDefault();
+    props.submitForm(inputs)
+  }
+
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs(inputsVal => ({ ...inputsVal, [name]: value }))
+  }
 
   return (
     <>
-      <form>
-        <label for="initInvestment">INITIAL INVEMENT</label>
+      <form onSubmit={submitForm}>
+        <label htmlFor="initInvestment">INITIAL INVEMENT</label>
         <br></br>
-        <input type="text" id="initInvestment" name="initInvestment"></input>
-        <button type="button" onClick={props.submitForm}>
+        <input type="number" id="initInvestment" name="initInvestment" value={inputs.initInvestment || ""}
+          onChange={handleChange}></input>
+        <br></br>
+        <label htmlFor="annualInvestment">ANNUAL INVEMENT</label>
+        <br></br>
+        <input type="number" id="annualInvestment" name="annualInvestment" value={inputs.annualInvestment || ""}
+          onChange={handleChange}></input>
+        <br></br>
+        <label htmlFor="expectedReturn">EXPECTED RETURN</label>
+        <br></br>
+        <input type="number" id="expectedReturn" name="expectedReturn" value={inputs.expectedReturn || ""}
+          onChange={handleChange}></input>
+        <br></br>
+        <label htmlFor="duration">DURATION</label>
+        <br></br>
+        <input type="number" id="duration" name="duration" value={inputs.duration || ""}
+          onChange={handleChange}></input>
+
+        <button type="submit">
           Submit
         </button>
       </form>
